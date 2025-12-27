4b443bc593fb7eb92a0853a542291503a7f392b2
Bota-Bonk <Bota-Bonk@users.noreply.github.com>
2025-12-27 00:50:49 +0000

    [ci] deploy site 83f7c91ac4cd48d96a90454c92ab5310627371df

---
+++ assets/use-pagetitle-CclU7xj1.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-Dwg_F9aS.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-bLnIoIXi.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-CEo5Z2dE.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
